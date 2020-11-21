a2bdff807de7d25c0dd4f51c1c39b8179bcd0269
Race_Condition <40449073+RogueWorm@users.noreply.github.com>
2020-11-21 15:49:17 -0500

    Update NJStateMuseum.java

--- app/src/main/java/com/example/pa5/NJStateMuseum.java
+++ app/src/main/java/com/example/pa5/NJStateMuseum.java
@@ -1,7 +1,10 @@
 package com.example.pa5;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -10,8 +13,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class NJStateMuseum extends AppCompatActivity {
 
+    private int SENIOR_PRICE = 17;
+    private int STUDENT_PRICE = 12;
+    private int ADULT_PRICE = 25;
+
     private ImageView MuseumPicture;
     private TextView name;
+    private Button ticketPrices;
+    private TextView ticketCost;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,9 +42,20 @@ public class NJStateMuseum extends AppCompatActivity {
         spinnerStudent.setAdapter(adapter);
         spinnerAdult.setAdapter(adapter);
 
+        ticketPrices = findViewById(R.id.calculateButton);
+
+        ticketPrices.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Double price = (SENIOR_PRICE * Double.parseDouble(spinner.getSelectedItem().toString()));
+                price +=  (STUDENT_PRICE * Double.parseDouble(spinnerStudent.getSelectedItem().toString()));
+                price += (ADULT_PRICE * Double.parseDouble(spinnerAdult.getSelectedItem().toString()));
+
+                ticketCost = findViewById(R.id.price_amount);
+                ticketCost.setText(price.toString());
+            }
+        });
+
+
     }
-
-
-
-
 }
