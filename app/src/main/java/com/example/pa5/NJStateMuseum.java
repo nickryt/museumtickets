696825856bfbbddb23efa53d421fc8fdedc431f5
Race_Condition <40449073+RogueWorm@users.noreply.github.com>
2020-11-21 15:59:12 -0500

    working

--- app/src/main/java/com/example/pa5/NJStateMuseum.java
+++ app/src/main/java/com/example/pa5/NJStateMuseum.java
@@ -1,36 +1,27 @@
 package com.example.pa5;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
-<<<<<<< HEAD
 import android.widget.Button;
-import android.widget.ImageView;
-=======
 import android.widget.ImageButton;
->>>>>>> 83777ed80bc9024140424142771816a6d53034d8
 import android.widget.Spinner;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NJStateMuseum extends AppCompatActivity {
 
-<<<<<<< HEAD
+
     private int SENIOR_PRICE = 17;
     private int STUDENT_PRICE = 12;
     private int ADULT_PRICE = 25;
 
-    private ImageView MuseumPicture;
-    private TextView name;
     private Button ticketPrices;
     private TextView ticketCost;
-=======
+
     private ImageButton MuseumPicture;
     private TextView name;
     private ImageButton backButton;
->>>>>>> 83777ed80bc9024140424142771816a6d53034d8
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,9 +43,8 @@ public class NJStateMuseum extends AppCompatActivity {
         spinnerStudent.setAdapter(adapter);
         spinnerAdult.setAdapter(adapter);
 
-<<<<<<< HEAD
         ticketPrices = findViewById(R.id.calculateButton);
-=======
+
         backButton = findViewById(R.id.backButton);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,21 +53,17 @@ public class NJStateMuseum extends AppCompatActivity {
             }
         });
 
-    }
->>>>>>> 83777ed80bc9024140424142771816a6d53034d8
-
         ticketPrices.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Double price = (SENIOR_PRICE * Double.parseDouble(spinner.getSelectedItem().toString()));
-                price +=  (STUDENT_PRICE * Double.parseDouble(spinnerStudent.getSelectedItem().toString()));
-                price += (ADULT_PRICE * Double.parseDouble(spinnerAdult.getSelectedItem().toString()));
+        @Override
+        public void onClick (View v) {
+            Double price = (SENIOR_PRICE * Double.parseDouble(spinner.getSelectedItem().toString()));
+            price += (STUDENT_PRICE * Double.parseDouble(spinnerStudent.getSelectedItem().toString()));
+            price += (ADULT_PRICE * Double.parseDouble(spinnerAdult.getSelectedItem().toString()));
 
-                ticketCost = findViewById(R.id.price_amount);
-                ticketCost.setText(price.toString());
+
+            ticketCost = findViewById(R.id.price_amount);
+            ticketCost.setText("$" + String.format("%.2f", price));
             }
         });
-
-
     }
 }
